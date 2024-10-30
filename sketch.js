996809d0700043b6c0ba3051163509845f937091
Koketso19 <bigshowbiggy73@gmail.com>
2024-10-30 22:18:06 +0200

    sketch

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ board.style.gridTampleRows = 'repeat($(size) , 1fr )'
 
 for (let i = 0; i < 256; i++){
     let square = document.createElement("div");
+    square.addEventListener("mouseover" , colorSquare);
     square.style.backgroundColor = "blue";
     board.insertAdjacentElement("beforeend", square);
 }
@@ -16,6 +17,15 @@ for (let i = 0; i < 256; i++){
 populateBoard(16);
 
 function chanceSize(input) {
-    if(input <2)
-    populateBoard(input);
+    if(input >2 && inut <=100){
+        populateBoard(input);
+
+    } else{
+        console.log("too many squares");
+    }
+    
+}
+
+function colorSquare() {
+    this.style.background = "black";
 }
